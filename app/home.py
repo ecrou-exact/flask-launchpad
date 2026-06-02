8e51cac9c78abcb5711c63025db2631baa1fdb78
ecrou-exact <geffetheo@gmail.com>
2026-06-02 14:57:20 +0200

    chg: [refactor] remove unused routes and templates; clean up navigation structure

--- app/home.py
+++ app/home.py
@@ -10,21 +10,5 @@ home_blueprint = Blueprint(
 
 @home_blueprint.route("/")
 def home():
-    session['ui_version'] = 1  # to set the version to have on each template the good base_1 or 2.....
     return render_template("home.html")
 
-@home_blueprint.route("/2")
-def home_2():
-    session['ui_version'] = 2  
-    return render_template("home_2.html")
-
-@home_blueprint.route("/3")
-def home_3():
-    session['ui_version'] = 3  
-    return render_template("home_3.html")
-
-
-# @home_blueprint.route("/3")
-# def home_3():
-#     session['ui_version'] = 3  
-#     return render_template("home_3.html") if you want to add a 3rd version....
